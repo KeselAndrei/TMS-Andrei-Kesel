f88af9db05db4393f571af617f10fe95eea784da
Андрей <KESELTMS@gmail.com>
2021-03-28 22:03:33 +0300

    Исправленный код

--- src/Task1.java
+++ src/Task1.java
@@ -1,24 +1,20 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 public class Task1 {
     public static void main(String[] args) throws IOException {
-        int count = 0;
-        int a = (int) Math.random()*16+5;
-        System.out.println("Ведите "+ a + " " + "целых чисел: ");
-        args = new String[a];
+        int a = 5;
+        System.out.println("Ведите " + a + " целых чисел:");
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            for (int i = 0;i< args.length;i++) {
-                args[i] = reader.readLine();
+        int count = 0;
+            for (int i = 0; i < a; i++) {
+                String inputLine = reader.readLine();
+                if (Integer.parseInt(inputLine) > 0) {
+                    count++;
+                }
             }
-        for (int i = 0;i< args.length;i++) {
-            if(Integer.parseInt(args[i])>0){
-                count++;
-            }
-        }
-        System.out.println(count);
+        System.out.println("Количество положительных чисел = " + count);
     }
 }
 
