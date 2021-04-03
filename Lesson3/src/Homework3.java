fbfff4c65e1e7ab8732064390ce258e9ff390be1
Андрей <KESELTMS@gmail.com>
2021-04-03 20:45:46 +0300

    Homework

---
+++ Lesson3/src/Homework3.java
@@ -0,0 +1,177 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Homework3 {
+    public static void main(String[] args) {
+        printArray();
+        System.out.println(operation(1));
+        System.out.println(operation(0));
+        System.out.println(calculateCountOfOddElementsInMatrix(new int[]{1, 2, 3, 4, 5, 6}));
+        calculateSumOfDiagonalElements();
+        countDevs(103);
+        foobar(6);
+        foobar(10);
+        foobar(15);
+        printMatrix();
+        printPrimeNumbers();
+    }
+
+    private static void printArray() {
+        Scanner scanner = new Scanner(System.in);
+        int number;
+        do {
+            System.out.println("Введите положительное целое число");
+            while (!scanner.hasNextInt()) {
+                System.out.println("Это не число!!!");
+                scanner.next();
+            }
+            number = scanner.nextInt();
+        } while (number <= 0);
+        int[] array = new int[number];
+        for (int i = 0; i < array.length; i++) {
+            Random random = new Random();
+            array[i] = random.nextInt(100);
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static int operation(int number) {
+        if (number > 0) {
+            return number += 1;
+        } else if (number < 0) {
+            return number -= 2;
+        } else {
+            return number = 10;
+        }
+    }
+
+    public static int calculateCountOfOddElementsInMatrix(int[] ints) {
+        int count = 0;
+        for (int i : ints) {
+            if (i % 2 != 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void countDevs(int count) {
+        String programer = " програмист";
+        if (count % 10 == 1 && count != 11) {
+            System.out.println(count + programer);
+        } else if (count % 10 == 2 && count != 12 || count % 10 == 3 && count != 13 || count % 10 == 4 && count != 14) {
+            System.out.println(count + programer + "а");
+        } else {
+            System.out.println(count + programer + "ов");
+        }
+    }
+
+    public static void foobar(int number) {
+        if (number % 3 == 0 && number % 5 != 0) {
+            System.out.println("foo");
+        } else if (number % 5 == 0 && number % 3 != 0) {
+            System.out.println("bar");
+        } else if (number % 3 == 0 && number % 5 == 0) {
+            System.out.println("foobar");
+        }
+    }
+
+    public static void calculateSumOfDiagonalElements() {
+        Scanner heightScaner = new Scanner(System.in);
+        Scanner widthScaner = new Scanner(System.in);
+        int height;
+        int width;
+        do {
+            System.out.println("Введите положительное целое число");
+            while (!widthScaner.hasNextInt() && !heightScaner.hasNext() || !widthScaner.hasNextInt() || !heightScaner.hasNext()) {
+                System.out.println("Это не число!!!");
+                widthScaner.next();
+                heightScaner.next();
+            }
+            while (!widthScaner.hasNextInt()) {
+                System.out.println("Это не число!!!");
+                widthScaner.next();
+            }
+            while (!heightScaner.hasNextInt()) {
+                System.out.println("Это не число!!!");
+                heightScaner.next();
+            }
+            width = widthScaner.nextInt();
+            height = heightScaner.nextInt();
+        } while (width <= 0 && height <= 0 || width <= 0 || height <= 0);
+        int[][] array = new int[width][height];
+        Random random = new Random();
+        int summaOfDiagonal = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = random.nextInt(100);
+                System.out.print(array[i][j] + " ");
+                if (i == j) {
+                    summaOfDiagonal += array[i][j];
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("Сумма элементов по диагонали = " + summaOfDiagonal);
+    }
+
+    public static void printMatrix() {
+        Scanner heightScaner = new Scanner(System.in);
+        Scanner widthScaner = new Scanner(System.in);
+        int height;
+        int width;
+        do {
+            System.out.println("Введите положительное целое число");
+            while (!widthScaner.hasNextInt() && !heightScaner.hasNext() || !widthScaner.hasNextInt() || !heightScaner.hasNext()) {
+                System.out.println("Это не число!!!");
+                widthScaner.next();
+                heightScaner.next();
+            }
+            while (!widthScaner.hasNextInt()) {
+                System.out.println("Это не число!!!");
+                widthScaner.next();
+            }
+            while (!heightScaner.hasNextInt()) {
+                System.out.println("Это не число!!!");
+                heightScaner.next();
+            }
+            width = widthScaner.nextInt();
+            height = heightScaner.nextInt();
+        } while (width <= 0 && height <= 0 || width <= 0 || height <= 0);
+        int[][] array = new int[width][height];
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = random.nextInt(100);
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (array[i][j] % 3 == 0 && array[i][j] % 7 == 0 || array[i][j] % 3 == 0 && array[i][j] % 7 != 0) {
+                    System.out.print("+ ");
+                } else if (array[i][j] % 7 == 0 && array[i][j] % 3 != 0) {
+                    System.out.print("- ");
+                } else {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printPrimeNumbers() {
+        for (int i = 2; i < 1000; i++) {
+            int count = 0;
+            for (int j = 2; j <= i; j++) {
+                if (i % j == 0) {
+                    count++;
+                }
+            }
+            if (count == 1)
+                System.out.print(i + " ");
+        }
+    }
+}
---
+++ Lesson3/src/Homework3Test.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Homework3Test {
+
+    @Test
+    void operation() {
+        Assertions.assertEquals(3, Homework3.operation(2));
+        Assertions.assertEquals(10, Homework3.operation(0));
+        Assertions.assertEquals(-4, Homework3.operation(-2));
+        Assertions.assertEquals(-100, Homework3.operation(-98));
+        Assertions.assertEquals(101, Homework3.operation(100));
+    }
+
+    @Test
+    void calculateCountOfOddElementsInMatrix() {
+        Assertions.assertEquals(0, Homework3.calculateCountOfOddElementsInMatrix(new int[]{2, 4, 6, 8, 10}));
+        Assertions.assertEquals(1, Homework3.calculateCountOfOddElementsInMatrix(new int[]{1, 4, 6, 8, 10}));
+        Assertions.assertEquals(2, Homework3.calculateCountOfOddElementsInMatrix(new int[]{-1, 2, -3, 4, 6, 8}));
+    }
+}
