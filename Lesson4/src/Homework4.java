04fe79078bd2c5a56e285c92245030911afd1cf3
Андрей <KESELTMS@gmail.com>
2021-04-10 13:33:33 +0300

    С треугольниками

--- Lesson4/src/Homework4.java
+++ Lesson4/src/Homework4.java
@@ -19,7 +19,11 @@ public class Homework4 {
         countNumbers(500000000);
         countNumbers(0);
         differentElements(new int[]{0, 3, 46, 3, 2, 1, 2});
+        triangle(new String[4][4]);
     }
+
+//  1) Одноклеточная амеба каждые 3 часа делится на 2 клетки. Определить,
+//     сколько амеб будет через 3, 6, 9, 12,..., 24 часа
 
     public static void cellDivision() {
         int count = 1;
@@ -33,6 +37,9 @@ public class Homework4 {
         System.out.println();
     }
 
+//  2) Напишите реализацию метода summ(int a, int b), вычисляющий a*b, не пользуясь операцией
+//     умножения, где a и b целые числа, вызовите метод summ  в методе main и распечатайте на консоль.
+
     public static int summ(int a, int b) {
         int sum = 0;
         for (int i = 0; i < Math.abs(a); i++) {
@@ -44,6 +51,9 @@ public class Homework4 {
             return sum;
         }
     }
+
+//  5) Создайте массив из всех нечётных чисел от 1 до 100, выведите его на экран в строку,
+//     а затем этот же массив выведите на экран тоже в строку, но в обратном порядке (99 97 95 93 ... 7 5 3 1).
 
     public static void mass(int[] array) {
         System.out.println();
@@ -58,6 +68,10 @@ public class Homework4 {
         System.out.println();
         System.out.println();
     }
+
+//  6) Создайте массив из int[] mass = new int[12]; Рандомно заполните его значениями от 0 до 15.
+//     Определите какой элемент является в этом массиве максимальным и сообщите индекс его последнего вхождения в массив.
+//     Пример: {3,4,5,62,7,8,4,-5,7,62,5,1} Максимальный элемент 62, индекс его последнего вхождения в массив = 10
 
     public static void maxElement(int[] mass) {
         Random random = new Random();
@@ -80,6 +94,10 @@ public class Homework4 {
         System.out.println();
     }
 
+//   7) Создайте массив размера 20, заполните его случайными целыми чиселами из отрезка от 0 до 20.
+//      Выведите массив на экран в строку. Замените каждый элемент с нечётным индексом на ноль.
+//      Снова выведете массив на экран на отдельной строке.
+
     public static void array() {
         int[] array = new int[20];
         Random random = new Random();
@@ -97,6 +115,8 @@ public class Homework4 {
         System.out.println();
         System.out.println();
     }
+
+// 8) Найти максимальный элемент в массиве {4,5,0,23,77,0,8,9,101,2} и поменять его местами с нулевым элементом
 
     public static void maxElementChange() {
         int[] mass = new int[]{4, 5, 0, 23, 77, 0, 8, 9, 101, 2};
@@ -121,6 +141,15 @@ public class Homework4 {
         System.out.println("Максимальный элемент равен - " + max);
         System.out.println();
     }
+
+//  10) Создаём квадратную матрицу, размер вводим с клавиатуры.
+//      Заполняем случайными числами в диапазоне от 0 до 50. И выводим на консоль(в виде матрицы).
+//      Далее необходимо транспонировать матрицу(1 столбец станет 1-й строкой, 2-й столбец - 2-й строкой и т. д.)
+//      Пример:
+//      1 2 3 4      1 6 3 1
+//      6 7 8 9      2 7 3 5
+//      3 3 4 5      3 8 4 6
+//      1 5 6 7      4 9 5 7
 
     public static void printMatrix() {
         Scanner heightScaner = new Scanner(System.in);
@@ -159,6 +188,11 @@ public class Homework4 {
         System.out.println();
     }
 
+//  4) В переменную записываем число.
+//     Надо вывести на экран сколько в этом числе цифр и положительное оно или отрицательное.
+//     Например, Введите число: 5
+//     "5 - это положительное число, количество цифр = 1"
+
     public static void countNumbers(int a) {
         int count = (a == 0) ? 1 : 0;
         int b = a;
@@ -179,6 +213,8 @@ public class Homework4 {
         }
     }
 
+//  9) Проверить, различны ли все элементы массива, если не различны то вывести элемент повторяющийся
+
     public static void differentElements(int[] array) {
         System.out.println();
         Arrays.sort(array);
@@ -191,6 +227,73 @@ public class Homework4 {
                 System.out.print("Массив имеет повторяющиеся элементы " + array[i] + ", ");
                 count = 0;
             }
+        }
+        System.out.println();
+        System.out.println();
+    }
+
+//  3) Дан двухмерный массив размерностью 4 на 4, необходимо нарисовать четыре треугольника вида
+
+//        a)                  b)
+//              *        *
+//            * *        * *
+//          * * *        * * *
+//        * * * *        * * * *
+//
+//        c)                  d)
+//        * * * *        * * * *
+//          * * *        * * *
+//            * *        * *
+//              *        *
+
+    public static void triangle(String[][] array) {
+        System.out.println("a");
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (j >= array.length - 1 - i) {
+                    array[i][j] = "*";
+                    System.out.print(array[i][j]);
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("b");
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (i >= j) {
+                    array[i][j] = "*";
+                    System.out.print(array[i][j]);
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("c");
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (i <= j) {
+                    array[i][j] = "*";
+                    System.out.print(array[i][j]);
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("d");
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (j <= array.length - 1 - i) {
+                    array[i][j] = "*";
+                    System.out.print(array[i][j]);
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
         }
     }
 }
